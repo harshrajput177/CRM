035d19e70e1d16125d9d6c704b1ebf65cbfc8b52
harshrajput177 <harshrajput30411@gmail.com>
2026-01-02 15:49:23 +0530

    update some file

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 /* 🔥 CORS */
 app.use(cors({
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  origin: ["http://localhost:5173",  "https://crm-backend-fo3o.onrender.com"],
   credentials: true
 }));
 
